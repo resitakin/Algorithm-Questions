5fb08e3644da8612d0a01c8171c0049a57787330
resitakin <resitakin9@gmail.com>
2022-09-08 18:36:28 +0300

    JDBC

---
+++ VeriGetir/src/DbHelper.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbHelper {
+    private String pasword="145301";
+    private String userName="root";
+    private String dbUrl="jdbc:mysql://localhost:3306/world";
+    
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(dbUrl,userName,pasword);
+    
+    }
+    public void showErrorMesage(SQLException exception){
+        System.out.println("Error : "+exception.getMessage());
+        System.out.println("error code :"+exception.getErrorCode());
+    }
+}
